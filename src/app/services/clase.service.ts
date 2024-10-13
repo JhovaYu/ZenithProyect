2f2e9759bbffa48525aa41892c714724ad4c5721
JhovaYu <yuca.jhovanny@gmail.com>
2024-10-12 23:43:42 -0600

    v1.0.0.1.0

--- src/app/services/clase.service.ts
+++ src/app/services/clase.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { Clase } from '../models/clase.model';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
